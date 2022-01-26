247352b823624a642882708b1ad44d5b80888b76
James Chan <jhchan99@gmail.com>
2022-01-26 14:41:00 -0700

    created classes for 'director'

---
+++ director.py
@@ -0,0 +1,13 @@
+class Director:
+
+    def __init__(self):
+        pass
+
+    def show_card(self):
+        pass
+
+    def make_guess(self):
+        pass
+
+    def show_points(self):
+        pass
